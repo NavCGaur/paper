ac6af99903d27d34970e47599f9bd6869c930dc2
NavCGaur <vertigo1112@gmail.com>
2025-01-28 23:23:11 +0530

    baseURl made suitable for both

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const paperApi = createApi({
   reducerPath: 'paperApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_BASE_URL }),
   endpoints: (builder) => ({
     generatePaper: builder.mutation({
       query: (paperDetails) => ({
